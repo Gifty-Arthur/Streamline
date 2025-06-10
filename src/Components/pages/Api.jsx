f45653537ef7ad4f274f5269f97689fdffc97062
Gifty Arthur <giftyarthur031@gmail.com>
2025-06-10 20:00:05 +0000

    update

--- src/Components/pages/Api.jsx
+++ src/Components/pages/Api.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import r1 from "../../assets/images/r1.png";
+
+import c2 from "../../assets/images/c2.png";
+import c3 from "../../assets/images/c3.png";
+import c4 from "../../assets/images/c4.png";
+import games from "../../assets/images/games.png";
+import Freq from "../pages/Freq";
 
 const MovieRow = () => {
   const [movies, setMovies] = useState([]);
@@ -31,81 +38,172 @@ const MovieRow = () => {
   };
 
   return (
-    <div className="relative w-full bg-black text-white p-6 overflow-hidden">
-      <h2 className="text-2xl font-bold mb-4">Trending Now</h2>
+    <div>
+      <div className="relative w-full bg-black text-white p-6 overflow-hidden">
+        <h2 className="text-2xl font-bold mb-4">Trending Now</h2>
+        {/* Scroll Buttons */}
+        <button
+          onClick={scrollLeft}
+          className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10 bg-black bg-opacity-50 rounded-full px-2 py-1 hover:bg-opacity-80"
+        >
+          ◀
+        </button>
+        <button
+          onClick={scrollRight}
+          className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10 bg-black bg-opacity-50 rounded-full px-2 py-1 hover:bg-opacity-80"
+        >
+          ▶
+        </button>
+        {/* Scrollable Movie Row */}
+        <div
+          ref={rowRef}
+          className="flex space-x-4 overflow-x-scroll"
+          style={{
+            scrollbarWidth: "none", // Firefox
+            msOverflowStyle: "none", // IE/Edge
+          }}
+        >
+          {/* Hide scrollbar for WebKit browsers */}
+          <style>{`
+            div::-webkit-scrollbar {
+              display: none;
+            }
+          `}</style>
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              onClick={() => setSelectedMovie(movie)}
+              className="min-w-[200px] transform hover:scale-105 transition-transform duration-300"
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className="rounded-md shadow-md"
+              />
+              <div className="p-2">
+                <h2 className="text-lg font-semibold">{movie.title}</h2>
+                <p className="text-sm text-gray-300">
+                  Rating: {movie.vote_average}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        {selectedMovie && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 z-50 flex items-center justify-center">
+            <div className="bg-gray-900 rounded-md p-6 w-[90%] md:w-[600px]">
+              <h2 className="text-2xl font-bold mb-2">{selectedMovie.title}</h2>
+              <p className="text-gray-300 text-sm mb-2">
+                Released: {selectedMovie.release_date}
+              </p>
+              <p className="text-gray-200 mb-4">{selectedMovie.overview}</p>
+              <div className="flex space-x-4">
+                <button className="bg-[#e50914] px-4 py-2 rounded-md font-semibold hover:bg-red-700">
+                  Get Started
+                </button>
+                <button
+                  onClick={() => setSelectedMovie(null)}
+                  className="border border-gray-400 px-4 py-2 rounded-md hover:bg-gray-700"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* cards */}
+      <div className="bg-black w-full h-full text-white p-6">
+        {/* Heading above the image */}
+        <h1 className="text-2xl font-bold mb-4">More Reasons to Join</h1>
 
-      {/* Scroll Buttons */}
-      <button
-        onClick={scrollLeft}
-        className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10 bg-black bg-opacity-50 rounded-full px-2 py-1 hover:bg-opacity-80"
-      >
-        ◀
-      </button>
-      <button
-        onClick={scrollRight}
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10 bg-black bg-opacity-50 rounded-full px-2 py-1 hover:bg-opacity-80"
-      >
-        ▶
-      </button>
-
-      {/* Scrollable Movie Row */}
-      <div
-        ref={rowRef}
-        className="flex space-x-4 overflow-x-scroll"
-        style={{
-          scrollbarWidth: "none", // Firefox
-          msOverflowStyle: "none", // IE/Edge
-        }}
-      >
-        {/* Hide scrollbar for WebKit browsers */}
-        <style>{`
-          div::-webkit-scrollbar {
-            display: none;
-          }
-        `}</style>
-
-        {movies.map((movie) => (
-          <div
-            key={movie.id}
-            onClick={() => setSelectedMovie(movie)}
-            className="min-w-[200px] transform hover:scale-105 transition-transform duration-300"
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-              className="rounded-md shadow-md"
-            />
-            <div className="p-2">
-              <h2 className="text-lg font-semibold">{movie.title}</h2>
-              <p className="text-sm text-gray-300">
-                Rating: {movie.vote_average}
+        {/* Image container */}
+        <div className="flex md:flex-row justify-between items-center flex-col">
+          <div className="relative w-fit z-10 mt-10">
+            {/* First Image */}
+            <img src={r1} alt="" className="w-[250px] h-auto rounded-2xl" />
+            {/* Text over image */}
+            <div className="absolute top-6 left-3 text-white">
+              <h2 className="text-2xl font-Poppins font-bold">
+                Enjoy on your TV
+              </h2>
+              <p className="text-[16px] font-normal mt-6 text-[#a7a7b1]  -ml-3">
+                Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+                Blu-ray players, and more.
               </p>
             </div>
+            {/* Second Image at bottom-right */}
+            <img
+              src={c2}
+              alt=""
+              className="absolute bottom-4 right-4 w-[54px]"
+            />
           </div>
-        ))}
-      </div>
-      {selectedMovie && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 z-50 flex items-center justify-center">
-          <div className="bg-gray-900 rounded-md p-6 w-[90%] md:w-[600px]">
-            <h2 className="text-2xl font-bold mb-2">{selectedMovie.title}</h2>
-            <p className="text-gray-300 text-sm mb-2">
-              Released: {selectedMovie.release_date}
-            </p>
-            <p className="text-gray-200 mb-4">{selectedMovie.overview}</p>
-            <div className="flex space-x-4">
-              <button className="bg-[#e50914] px-4 py-2 rounded-md font-semibold hover:bg-red-700">
-                Get Started
-              </button>
-              <button
-                onClick={() => setSelectedMovie(null)}
-                className="border border-gray-400 px-4 py-2 rounded-md hover:bg-gray-700"
-              >
-                Close
-              </button>
+          {/* second  */}
+          <div className="relative w-fit z-10 mt-10">
+            <img src={r1} alt="" className="w-[250px] h-auto rounded-2xl" />
+
+            <div className="absolute  top-6 left-2 text-white">
+              <h2 className="text-2xl font-Poppins font-bold tex">
+                Download your shows to watch offline
+              </h2>
+              <p className="text-[16px] font-normal mt-6 text-[#a7a7b1]  -ml-3">
+                Save your favorites easily and always have something to watch.
+              </p>
             </div>
+
+            <img
+              src={c3}
+              alt=""
+              className="absolute bottom-4 right-4 w-[54px]"
+            />
+          </div>
+
+          {/* third */}
+          <div className="relative w-fit z-10 mt-10">
+            {/* First Image */}
+            <img src={r1} alt="" className="w-[250px] h-auto rounded-2xl" />
+            {/* Text over image */}
+            <div className="absolute  top-5 left-3 text-white">
+              <h2 className="text-2xl font-Poppins font-bold tex">
+                Watch everywhere
+              </h2>
+              <p className="text-[16px] font-normal mt-6 text-[#a7a7b1]  -ml-3">
+                Stream unlimited movies and TV shows on your phone, tablet,
+                laptop, and TV.
+              </p>
+            </div>
+
+            <img
+              src={c4}
+              alt=""
+              className="absolute bottom-4 right-4 w-[54px]"
+            />
+          </div>
+          {/* fourth */}
+          <div className="relative w-fit z-10 mt-10">
+            {/* First Image */}
+            <img src={r1} alt="" className="w-[250px] h-auto rounded-2xl" />
+            {/* Text over image */}
+            <div className="absolute  top-5 left-2 text-white">
+              <h2 className="text-2xl font-Poppins font-bold tex">
+                Create profiles for kids
+              </h2>
+              <p className="text-[16px] font-normal mt-6 text-[#a7a7b1]  -ml-3">
+                Send kids on adventures with their favorite characters in a
+                space made just for them — free with your membership.
+              </p>
+            </div>
+
+            <img
+              src={games}
+              alt=""
+              className="absolute bottom-4 right-4 w-[54px]"
+            />
           </div>
         </div>
-      )}
+      </div>
+      <Freq />
     </div>
   );
 };
---
+++ src/Components/pages/Freq.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
+
+const Freq = () => {
+  const [activeQuestion, setActiveQuestion] = useState(null);
+  const questions = [
+    {
+      id: 1,
+      question: "What is Netflix?",
+      answer:
+        "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!",
+    },
+    {
+      id: 2,
+      question: "How much does Netflix cost?",
+      answer:
+        "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from US$2.99 to US$9.99 a month. No extra costs, no contracts.",
+    },
+    {
+      id: 3,
+      question: "Where can i watch?",
+      answer:
+        "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.You can also download your favorite shows with the iOS or Android app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+    },
+    {
+      id: 4,
+      question: "How do i cancel?",
+      answer:
+        "Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+    },
+
+    {
+      id: 5,
+      question: "What can i watch on Netflix?",
+      answer:
+        "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+    },
+
+    {
+      id: 6,
+      question: "Is Netflix good for Kids?",
+      answer:
+        "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.",
+    },
+  ];
+  return (
+    <div className=" h-screen bg-black p-5 ">
+      <div className="">
+        <h2 className="text-white mt-10 text-3xl">Frequently Asked Question</h2>
+        {questions.map((q) => (
+          <div key={q.id} className="mb-4 last:mb-0">
+            <button
+              className="w-[1174px] h-[84px] text-left text-xl focus:outline-none p-3 bg-[#2d2d2d] shadow-md flex justify-between item-center mt-2"
+              onClick={() =>
+                setActiveQuestion(activeQuestion === q.id ? null : q.id)
+              }
+            >
+              {q.question}
+              {activeQuestion === q.id ? <FaMinusCircle /> : <FaPlusCircle />}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Freq;
